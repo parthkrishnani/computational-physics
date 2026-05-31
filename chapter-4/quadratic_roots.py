a261f7dcf4a09c826d56621846e9c073d3c280bc
Parth Krishnani <krishnaniparth@gmail.com>
2026-05-31 21:46:25 +0530

    Improved accuracy by using alternative numerical methods

--- chapter-4/quadratic_roots.py
+++ chapter-4/quadratic_roots.py
@@ -2,9 +2,7 @@ import numpy as np
 a=0.001
 b=1000
 c=0.001
-x1=(-b+np.sqrt(b**2-4*a*c))/(2*a)
-x1_=2*c/(-b-np.sqrt(b**2-4*a*c))
+x1=2*c/(-b-np.sqrt(b**2-4*a*c))
 x2=(-b-np.sqrt(b**2-4*a*c))/(2*a)
-x2_=2*c/(-b+np.sqrt(b**2-4*a*c))
-print(x1,x1_)
-print(x2,x2_)
+print(x1)
+print(x2)
