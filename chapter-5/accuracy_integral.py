76d85e6d4e912dba0ee233f333e9dcc6b127eb0e
Parth Krishnani <krishnaniparth@gmail.com>
2026-06-08 02:52:23 +0530

    Improved accuracy by calculating for higher values of N

--- chapter-5/accuracy_integral.py
+++ chapter-5/accuracy_integral.py
@@ -1,25 +1,24 @@
 import numpy as np
-n=np.zeros(13)
-for i in range(13):
+n=np.zeros(15)
+for i in range(15):
     n[i]=2**(i)
 def f(x):
     return (np.sin(10*np.sqrt(x)))**2
 a=0
 b=2
-I=np.zeros(13)
-h=np.zeros(13)
-for i in range(13):
+I=np.zeros(15)
+h=np.zeros(15)
+for i in range(15):
     h[i]=(b-a)/n[i]
-err=np.zeros(13)
+err=np.zeros(15)
 I[0] = h[0] * (0.5*f(a) + 0.5*f(b))
 print('n=1, Integral=',I[0])
-for i in range(1,13):
+for i in range(1,15):
     I[i]=0.5*I[i-1]
     for j in range(1,int(n[i]),2):
         I[i]+=f(a+j*h[i])
     I[i]*=h[i]
     print('n=',int(n[i]),', Integral=',I[i])
-for i in range(1,13):
+for i in range(1,15):
     err[i]=abs(I[i]-I[i-1])/3
     print('n=',int(n[i]),', Error estimate=',err[i])
-
