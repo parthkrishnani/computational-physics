7ab59842e7a4c2436a5f37a5129a7464b5b6dc5f
Parth Krishnani <krishnaniparth@gmail.com>
2026-06-19 18:39:37 +0530

    Updated algorithm to allow pivots for LU Decomposition

--- chapter-6/lu_decomp.py
+++ chapter-6/lu_decomp.py
@@ -1,10 +1,18 @@
 import numpy as np
-A=np.array([[2,1,4,1],
+A=np.array([[0,1,4,1],
             [3,4,-1,-1],
             [1,-4,1,5],
             [2,-2,1,3]],float)
 b=np.array([-4,3,9,7],float)
 N=len(b)
+for m in range(N):
+    if A[m,m]==0:
+        if m<N-1:
+            A[[m, m+1]] = A[[m+1, m]]
+            b[[m,m+1]] = b[[m+1,m]]
+        else:
+            A[[m, m-1]] = A[[m-1, m]]
+            b[[m,m-1]] = b[[m-1,m]]
 U=A.copy()
 L=np.zeros([N,N])
 for i in range(N):
