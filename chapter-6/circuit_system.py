f5c83fafbfbae753de5a292d713551983c3a3f04
Parth Krishnani <krishnaniparth@gmail.com>
2026-06-19 16:16:24 +0530

    Updated Kirchoff solver

--- chapter-6/circuit_system.py
+++ chapter-6/circuit_system.py
@@ -4,6 +4,19 @@ A=np.array([[4,1,1,1],
             [-1,0,-1,3],
             [-1,3,0,-1]],float)
 v_pl=5
-b=np.array([v_pl,0,v_pl,0],float)
-x=np.linalg.solve(A,b)
+v=np.array([v_pl,0,v_pl,0],float)
+N=len(v)
+for m in range(N):
+    d=A[m,m]
+    A[m,:]/=d
+    v[m]/=d
+    for j in range(m+1,N):
+        mul=A[j,m]
+        A[j,:]-=mul*A[m,:]
+        v[j]-=mul*v[m]
+x=np.empty(N,float)
+for i in range(N-1,-1,-1):
+    x[i]=v[i]
+    for k in range(i+1,N):
+        x[i]-=A[i,k]*x[k]
 print(x)
